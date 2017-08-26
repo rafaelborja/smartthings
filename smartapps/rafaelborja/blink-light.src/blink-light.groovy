f08a53a41ebb766456d26d0ffb8d2b6ec32a4b40
Rafael Borja <rafaelborja@gmail.com>
2017-08-25 23:29:19 -0400

    Initial version

---
+++ smartapps/rafaelborja/blink-light.src/blink-light.groovy
@@ -0,0 +1,126 @@
+/**
+ *  Blink Light
+ *
+ *  Copyright 2017 Rafael Borja
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Blink Light",
+    namespace: "rafaelborja",
+    author: "Rafael Borja",
+    description: "Blink Light",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+	section("Title") {
+		// TODO: put inputs here
+	}
+    section("Turn on when motion detected:") {
+        input "themotion", "capability.button", required: true, title: "Where?"
+    }
+    section("Turn on this light") {
+        input "switches", "capability.switch", required: true, multiple: true
+    }
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	// TODO: subscribe to attributes, devices, locations, etc.
+    subscribe(themotion, "button.pushed", motionDetectedHandler)
+}
+
+// TODO: implement event handlers
+
+def motionDetectedHandler(evt) {
+    log.debug "motionDetectedHandler called: $evt"
+    
+
+    flashLights()
+    
+    switches.on()
+    
+    
+ /*   switch: off
+le/vel: 0
+hue: 14
+saturation: 100
+color: #0EC5FF
+colorTemperature: 6536
+unreachable: 0
+colorMode: Color
+colorName: Gold
+switchColor: off
+    */
+    
+
+}
+
+private flashLights() {
+	def doFlash = true
+	def onFor = onFor ?: 1000
+	def offFor = offFor ?: 1000
+	def numFlashes = numFlashes ?: 3
+
+	log.debug "LAST ACTIVATED IS: ${state.lastActivated}"
+	if (state.lastActivated) {
+		def elapsed = now() - state.lastActivated
+		def sequenceTime = (numFlashes + 1) * (onFor + offFor)
+		doFlash = elapsed > sequenceTime
+		log.debug "DO FLASH: $doFlash, ELAPSED: $elapsed, LAST ACTIVATED: ${state.lastActivated}"
+	}
+
+	if (doFlash) {
+		log.debug "FLASHING $numFlashes times"
+		state.lastActivated = now()
+		log.debug "LAST ACTIVATED SET TO: ${state.lastActivated}"
+		def initialActionOn = switches.collect{it.currentSwitch != "on"}
+		def delay = 0L
+		numFlashes.times {
+			log.trace "Switch on after  $delay msec"
+			switches.eachWithIndex {s, i ->
+				if (initialActionOn[i]) {
+					s.on(delay: delay)
+				}
+				else {
+					s.off(delay:delay)
+				}
+			}
+			delay += onFor
+			log.trace "Switch off after $delay msec"
+			switches.eachWithIndex {s, i ->
+				if (initialActionOn[i]) {
+					s.off(delay: delay)
+				}
+				else {
+					s.on(delay:delay)
+				}
+			}
+			delay += offFor
+		}
+	}
+}
